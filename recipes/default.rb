4c9681b7ae8337ff29882dae978fc38d811e6842
Mark Burton <mark@greensocs.com>
2012-11-05 16:11:20 +0100

    initial try and s/w build recipy

--- recipes/default.rb
+++ recipes/default.rb
@@ -1,1 +1,14 @@
-package "gem5"
+
+package "mercurial"
+package "scons"
+
+bash "get-and-compile-GEM5" do
+  cwd Chef::Config[:file_cache_path]
+  code <<-EOH
+    echo "Building in :"
+    pwd
+    hg clone http://repo.gem5.org/gem5/rev/f75ee4849c40
+    
+  EOH
+  creates "/usr/lib/gem5"
+end
