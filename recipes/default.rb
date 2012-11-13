908760d39b347fe60980ae17516e2ff65a3ef460
Mark Burton <mark@greensocs.com>
2012-11-13 15:44:03 +0100

    add patches

--- recipes/default.rb
+++ recipes/default.rb
@@ -9,8 +9,17 @@ package "m4"
 package "lua5.2"
 package "libgoogle-perftools-dev"
 
+include "chef-amba-kit"
+
+cookbook_file "/tmp/Patches.tgz" do
+  source "Patches.tgz"
+  mode "0644"
+end
+
+
 bash "get-and-compile-GEM5" do
   cwd Chef::Config[:file_cache_path]
+#  --- I think we should build here !!!!
   code <<-EOH
     mkdir -p /vagrant/gem5
     cd /vagrant/gem5
@@ -20,10 +29,21 @@ bash "get-and-compile-GEM5" do
     hg clone "http://repo.gem5.org/gem5"
     cd gem5
     hg checkout stable_2012_06_28
+
+    cp /tmp/Patches.tgz .
+    tar -zxf Patches.tgz
+
+    for file in Patches/*
+    do
+      patch << ${file}
+    done
     
     scons build/ARM/gem5.opt
 
-    
+#  now re-build the'systemc link files
+
+#  now do a big 'link'.
+
   EOH
   creates "/usr/lib/gem5"
 end
