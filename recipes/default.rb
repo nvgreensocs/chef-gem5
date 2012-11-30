d48e9e1ae2778c95f17baf605a29ee4c952af386
Mark Burton <mark@greensocs.com>
2012-11-30 14:41:06 +0100

    license

--- recipes/default.rb
+++ recipes/default.rb
@@ -1,3 +1,17 @@
+
+#  -------    CHEF-GEM5 --------
+
+# LICENSETEXT
+# 
+#   Copyright (C) 2012 : GreenSocs Ltd
+#       http://www.greensocs.com/ , email: info@greensocs.com
+# 
+# The contents of this file are subject to the licensing terms specified
+# in the file LICENSE. Please consult this file for restrictions and
+# limitations that may apply.
+# 
+# ENDLICENSETEXT
+
 package "mercurial"
 package "git"
 package "scons"
