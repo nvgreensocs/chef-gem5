3f5b5f3b156b313f0245ce337bf71c467039bf07
Mark Burton <mark@greensocs.com>
2013-02-07 13:40:17 +0100

    source the bash scripts

--- recipes/default.rb
+++ recipes/default.rb
@@ -135,7 +135,7 @@ ENV['http_proxy'] = Chef::Config[:http_proxy]
 ruby_block "compile-SYSTEMC-GEM5-ARM" do
   block do
     IO.popen( <<-EOH
-       for i in #{node[:prexix}/bash.profile.d/* ; do source $i ; done
+       for i in #{node[:prexix]}/bash.profile.d/* ; do source $i ; done
        cd #{node[:prefix]}/ModelLibrary/Gem5SystemC/
        scons
      EOH
